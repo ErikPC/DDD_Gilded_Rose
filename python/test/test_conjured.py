5e9f6945b021ad9df8367ee39b1bda548f3eac55
ErikPC <epardillo@cifpfboll.eu>
2022-01-28 23:14:54 +0100

    tests conjured

---
+++ python/test/test_conjured.py
@@ -0,0 +1,34 @@
+import pytest
+
+@pytest.mark.update_quality_conjured
+def test_update_quality_conjured():
+    conjured = Conjured("Conjured Mana Cake", 3, 6)
+    conjured.updateQuality()
+    assert 2 == conjured.getSell_in()
+    assert 4 == conjured.getquality()
+
+@pytest.mark.update_quality_conjured_just_expired
+def test_update_quality_conjured_just_expired():
+    conjured = Conjured("Conjured Mana Cake", 0, 6)
+    conjured.updateQuality()
+    assert -1 == conjured.getSell_in()
+    assert 2 == conjured.getQuality()
+
+@pytest.mark.update_quality_conjured_expired
+def test_update_quality_conjured_expiredU():
+    conjured = Conjured("Conjured Mana Cake", -1, 6)
+    conjured.updateQuality()
+    assert -2 == conjured.getSell_in()
+    assert 2 == conjured.getQuality()
+
+@pytest.mark.quality_min_ZERO
+def test_quality_min_ZERO():    
+    brie = Conjured("Conjured Mana Cake", 1, 1)
+    brie.updateQuality()
+    assert 0 == brie.getSell_in()
+    assert 0 == brie.getQuality()
+
+    brie = Conjured("Conjured Mana Cake", -1, 0)
+    brie.updateQuality()
+    assert -2 == brie.getSell_in()
+    assert 0 == brie.getQuality()

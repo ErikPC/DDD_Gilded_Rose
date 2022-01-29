49ecde7ac68641f0c61e1bb01ffd174004b9680b
ErikPC <epardillo@cifpfboll.eu>
2022-01-29 19:12:14 +0100

    pasar test NormalItem

---
+++ python/src/NormalItem.py
@@ -0,0 +1,24 @@
+from src.Item import Item
+from src.updateable import updateable
+
+class NormalItem(Item, updateable):
+    def __init__(self, name, sell_in, quality):
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+    
+    def getSell_in(self):
+        return self.sell_in
+    
+    def getQuality(self):
+        return self.quality
+    
+    def updateQuality(self):
+        if self.sell_in <= 0:
+            self.quality -= 2
+            self.sell_in -= 1
+        else:
+            self.quality -= 1
+            self.sell_in -= 1
+        if self.quality <= 0:
+            self.quality = 0

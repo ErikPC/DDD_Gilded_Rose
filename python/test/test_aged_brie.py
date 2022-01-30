e60823467e7024022f09d2db4a796f219f29bb65
ErikPC <epardillo@cifpfboll.eu>
2022-01-30 17:00:45 +0100

    fix bugs

--- python/test/test_aged_brie.py
+++ python/test/test_aged_brie.py
@@ -1,3 +1,4 @@
+from src.Aged_brie import AgedBrie
 import pytest
 
 
@@ -12,14 +13,16 @@ def test_update_quality_brie():
 def test_update_quality_expired():
     cheese = AgedBrie("Aged Brie", 0, 0)
     assert -1 == cheese.getSell_in()
-    assert 2 == cheese.getQuality
+    assert 2 == cheese.getQuality()
 
 @pytest.mark.max_quality
 def test_max_quality():
     cheese = AgedBrie("Aged Brie", -1, 50)
+    cheese.updateQuality()
     assert -2 == cheese.getSell_in()
-    assert 50 == cheese.getQuality
+    assert 50 == cheese.getQuality()
 
     cheese = AgedBrie("Aged Brie", -1, 49)
+    cheese.updateQuality()
     assert -2 == cheese.getSell_in()
     assert 50 == cheese.getQuality()

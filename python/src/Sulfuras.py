1ddcb3e869151031a0670e0ab2fffcb529756d63
ErikPC <epardillo@cifpfboll.eu>
2022-01-30 17:33:49 +0100

    refactor Sulfuras

--- python/src/Sulfuras.py
+++ python/src/Sulfuras.py
@@ -3,9 +3,7 @@ from src.updateable import updateable
 class Sulfuras(Item, updateable):
 
     def __init__(self, name , sell_in, quality):
-        self.name = name
-        self.sell_in = sell_in
-        self.quality = quality
+        Item.__init__(self, name, sell_in, quality)
     
     def getSell_in(self):
         return self.sell_in

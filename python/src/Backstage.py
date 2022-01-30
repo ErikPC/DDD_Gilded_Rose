538404207678eac407da17dc4381dd45311b9851
ErikPC <epardillo@cifpfboll.eu>
2022-01-30 16:37:26 +0100

    pasar casos test

---
+++ python/src/Backstage.py
@@ -0,0 +1,32 @@
+from src.Item import Item
+from src.updateable import updateable
+
+class Backstage(Item, updateable):
+
+    def __init__(self, name, sell_in, quality):
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+
+    def getSell_in(self):
+        return self.sell_in
+    
+    def getQuality(self):
+        return self.quality
+    
+    def updateQuality(self):
+        if self.sell_in > 10:
+            self.quality += 1
+        elif self.sell_in <= 5:
+            self.quality += 3
+        else:
+             self.quality += 2
+        if self.sell_in <= 0:
+            self.quality = 0
+        Backstage.maxQuality(self)
+        print(self.sell_in)
+        self.sell_in -= 1
+
+    def maxQuality(self):
+        if self.quality >= 50:
+            self.quality = 50

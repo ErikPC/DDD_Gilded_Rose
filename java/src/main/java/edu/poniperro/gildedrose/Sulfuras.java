74caae98d47737697703cb906e266e4093b39d6a
ErikPC <epardillo@cifpfboll.eu>
2022-02-01 02:01:22 +0100

    pasar casos test

---
+++ java/src/main/java/edu/poniperro/gildedrose/Sulfuras.java
@@ -0,0 +1,13 @@
+package edu.poniperro.gildedrose;
+
+public class Sulfuras extends Item implements updateable {
+
+    Sulfuras(String name, int sell_in, int quality) {
+        super(name, sell_in, quality);
+    }
+
+    @Override
+    public void updateQuality() {
+
+    }
+}

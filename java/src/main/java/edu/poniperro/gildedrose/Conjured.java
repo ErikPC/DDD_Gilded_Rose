1cc86f9c29b1202fc77d824ab3c8916029e7f0d1
ErikPC <epardillo@cifpfboll.eu>
2022-02-01 02:16:28 +0100

    pasar casos tes Conjured

---
+++ java/src/main/java/edu/poniperro/gildedrose/Conjured.java
@@ -0,0 +1,28 @@
+package edu.poniperro.gildedrose;
+
+public class Conjured extends Item implements updateable {
+
+    Conjured(String name, int sell_in, int quality) {
+        super(name, sell_in, quality);
+    }
+
+    @Override
+    public void updateQuality() {
+        if (getSell_in() <= 0) {
+            setQuality(getQuality() - 4);
+        } else {
+            setQuality(getQuality() - 2);
+        }
+        setSell_in();
+        checkQuality();
+    }
+
+    private void checkQuality() {
+        if (getQuality() >= 50) {
+            setQuality(50);
+        }
+        if (getQuality() <= 0) {
+            setQuality(0);
+        }
+    }
+}

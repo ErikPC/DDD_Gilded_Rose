4fd1d9014e0e597a5e7aa596705eba02e86058b9
ErikPC <epardillo@cifpfboll.eu>
2022-02-01 02:28:33 +0100

    pasar casos test

---
+++ java/src/main/java/edu/poniperro/gildedrose/Backstage.java
@@ -0,0 +1,33 @@
+package edu.poniperro.gildedrose;
+
+public class Backstage extends Item implements updateable {
+
+    Backstage(String name, int sell_in, int quality) {
+        super(name, sell_in, quality);
+    }
+
+    @Override
+    public void updateQuality() {
+        if (getSell_in() > 10) {
+            setQuality(getQuality() + 1);
+        } else if (getSell_in() <= 5) {
+            setQuality(getQuality() + 3);
+        } else {
+            setQuality(getQuality() + 2);
+        }
+        if (getSell_in() <= 0) {
+            setQuality(0);
+        }
+        setSell_in();
+        checkQuality();
+    }
+
+    private void checkQuality() {
+        if (getQuality() >= 50) {
+            setQuality(50);
+        }
+        if (getQuality() <= 0) {
+            setQuality(0);
+        }
+    }
+}

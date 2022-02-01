9d58bd1984aba47f839c6c89e9ee4c983084eda7
ErikPC <epardillo@cifpfboll.eu>
2022-02-01 01:32:31 +0100

    pasar caso test AgedBrie

--- java/src/main/java/gildedRose/AgedBrie.java
+++ java/src/main/java/gildedRose/AgedBrie.java
@@ -1,40 +1,27 @@
 package gildedRose;
 
-public class AgedBrie {
+public class AgedBrie extends Item implements updateable {
 
-	private final String name;
-	private int sell_in;
-	private int quality;
-
-	public AgedBrie(String name, int Sell_in, int quality) {
-		this.name = name;
-		this.sell_in = Sell_in;
-		this.quality = quality;
-	}
-
-	public String getName() {
-		return this.name;
-	}
-
-	public int getSell_in() {
-		return this.sell_in;
-	}
-
-	public int getQuality() {
-		return this.quality;
+	AgedBrie(String name, int sell_in, int quality) {
+		super(name, sell_in, quality);
 	}
 
 	public void updateQuality() {
-
+		if (getSell_in() <= 0) {
+			setQuality(getQuality() + 2);
+		} else {
+			setQuality(getQuality() + 1);
+		}
 		checkQuality();
+		setSell_in();
 	}
 
 	private void checkQuality() {
-		if (this.quality >= 50) {
-			this.quality = 50;
+		if (getQuality() >= 50) {
+			setQuality(50);
 		}
-		if (this.quality <= 0) {
-			this.quality = 0;
+		if (getQuality() <= 0) {
+			setQuality(0);
 		}
 	}
 }
